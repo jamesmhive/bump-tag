56a4473009e3941c16bdc8ea934cffa5d5484e3d
James MacManus <james.macmanus@introhive.com>
2023-03-01 18:44:32 -0400

    wip

--- .github/actions/release/index.js
+++ .github/actions/release/index.js
@@ -5,6 +5,8 @@ try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
+    const release = core.getInput('release');
+    console.log(`release = ${release}`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
