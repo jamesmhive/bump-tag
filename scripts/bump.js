31af4bc3e22ceb5e4b50c57ac60ba3821c655478
James MacManus <james.macmanus@introhive.com>
2023-03-04 11:23:25 -0400

    wip

--- scripts/bump.js
+++ scripts/bump.js
@@ -11,25 +11,29 @@ try {
     void main();
 } catch(error) {
     logError(error);
+    process.exit(1);
 }
 
 async function main() {
+    console.log(`\nbumpbot ${getWakeUpMessage()}...\n`);
+    void await ensurePrerequisites();
+
     const packages = await getWorkspacePackages();
     const response = await promptUser(packages, {
         onCancel: () => {
-            console.log('process cancelled by user.')
+            console.log(`\n${getExitExcuse()}`);
             process.exit(1);
         }
     });
 
     const packageInfo = packages.find((pkg) => pkg.name === response.packageName);
 
-    // await bump({
-    //     ...response,
-    //     packageInfo,
-    // });
+    await bump({
+        ...response,
+        packageInfo,
+    });
 
-    console.log('Done');
+    console.log('\nBumped!');
 }
 
 function getWorkingDirectory() {
@@ -44,14 +48,14 @@ function getWorkingDirectory() {
 async function promptUser(packages, options) {
     const packageChoices = packages.map((pkg) => ({
         title: pkg.nameNoScope,
-        description: pkg.description ?? pkg.name,
+        description: pkg.description || pkg.name,
         value: pkg.name
     }));
     return await prompts([
         {
             type: 'text',
-            name: 'branch',
-            message: `Main branch name`,
+            name: 'mainBranch',
+            message: `Main branch`,
             initial: `master`,
         },
         {
@@ -82,40 +86,117 @@ async function promptUser(packages, options) {
 }
 
 async function bump({
-    branch,
+    mainBranch,
     remote,
     releaseType,
     packageInfo,
 }) {
-    const isClean = ensureCleanBranch(branch, remote);
+    const isClean = ensureCleanBranch(mainBranch, remote);
     if (!isClean) {
-        return exitWithError('Could not ensure the local branch is clean.');
+        return exitWithError('Directory contains uncommitted changes.');
     }
 
-    console.log(`Getting latest from "${branch}"...`);
+    console.log(`Getting latest from "${mainBranch}"...`);
     await run('git', ['fetch']);
-    await run('git', ['pull']);
+    await run('git', ['checkout', mainBranch]);
+    await run('git', ['pull', remote, mainBranch]);
 
     console.log(`Running 'npm version' with "${releaseType}" on ${packageInfo.name}`);
-    const {stdout: nextVersion} = await run('npm', [
+    await run('npm', [
         'version',
         '--git-tag-version=false',
         releaseType
-    ]);
+    ], {
+        cwd: packageInfo.directory,
+    });
 
-    console.log(`Next version: ${nextVersion}`);
+    const {version: nextVersion} = await readPackageJson(packageInfo.file);
+    console.log(`Bump version: ${packageInfo.version} -> ${nextVersion}`);
 
-    const releaseBranchName = `bump-${packageInfo.nameNoScope}-${nextVersion}`;
-    const commitMessage = `bump ${releaseBranchName}`;
+    const bumpBranchName = `bump/${packageInfo.nameNoScope}-v${nextVersion}`;
+    const commitMessage = `bump! ${packageInfo.nameNoScope}-v${nextVersion}`;
 
-    console.log(`Creating release branch: ${releaseBranchName}`);
-    await run('git', ['branch', releaseBranchName]);
-    await run('git', ['checkout', releaseBranchName]);
+    // TODO: check if the branch already exists on remote
+    console.log(`Creating release branch: ${bumpBranchName}`);
+    await run('git', ['checkout', '-b', bumpBranchName]);
     await run('git', ['add', '--all']);
     await run('git', ['commit', '-m', commitMessage]);
-    await run('git', ['push', remote, releaseBranchName]);
-    await run('git', ['checkout', branch]);
+    await run('git', ['push', '-u', remote, bumpBranchName]);
+    await run('git', ['pull']);
+    await run('git', ['checkout', mainBranch]);
 
+    console.log('Cleaning up.');
+    await run('git', ['branch', '-d', bumpBranchName]);
+
+    console.log('Preparing pull request');
+    const bumpbotLabel = 'bumpbot';
+
+    await run('gh', [
+        'label',
+        'create',
+        bumpbotLabel,
+        '--description',
+        'Pull request created by bumpbot',
+        '--color',
+        '6EE7B7',
+        '--force',
+    ]);
+
+    await run('gh', [
+        'pr',
+        'create',
+        '--head',
+        bumpBranchName,
+        '--title',
+        `:arrow_double_up: BUMP! ${packageInfo.nameNoScope} v${nextVersion} (${releaseType})`,
+        '--label',
+        bumpbotLabel,
+        '--body',
+        renderPullRequestBody({
+            packageName: packageInfo.name,
+            previousVersion: packageInfo.version,
+            releaseType,
+            nextVersion,
+        }),
+    ], {
+        stdio: 'inherit'
+    });
+
+}
+
+function renderPullRequestBody({
+    packageName,
+    releaseType,
+    nextVersion,
+    previousVersion,
+}) {
+    const code = (text) => `\`${text}\``
+    return [
+        `### ${packageName}`,
+        `**Release type:** ${code(releaseType)}`,
+        `**Next version:** ${code(nextVersion)}`,
+        `**Previous version:** ${code(previousVersion)}`,
+        `\n\n\n_Pull request created by bumpbot_ :godmode:`
+    ].join('\n\n');
+}
+async function ensurePrerequisites() {
+    if (!(await isGitHubCliInstalled())) {
+        exitWithError('GitHub CLI is not installed.\nRun "brew install gh"\nOr download from https://cli.github.com/');
+        return;
+    }
+    if (await hasUncommittedChanges()) {
+        exitWithError('You have uncommitted changes. Commit your changes before running bump.');
+        return;
+    }
+}
+
+async function isGitHubCliInstalled() {
+    try {
+        const {stderr} = await run('gh', ['--version']);
+        return !stderr;
+    } catch(error) {
+        return false;
+    }
 }
 
 async function getHashFor(branch) {
@@ -156,20 +237,18 @@ async function ensureCleanBranch(branch, remote) {
 }
 
 async function getWorkspacePackages() {
-    const packages = [];
-
     const rootPackageJsonPath = path.join(__workdir, 'package.json');
     const root = await readPackageJson(rootPackageJsonPath);
 
-    packages.push(createPackageEntry({
+    const rootPackageEntry = createPackageEntry({
         root: true,
         packageJson: root,
         directory: __workdir,
         file: rootPackageJsonPath,
-    }));
+    });
 
     if (!Array.isArray(root.workspaces)) {
-        return packages;
+        return [rootPackageEntry];
     }
 
     const packageJsonPaths = await Promise.all(root.workspaces.map(async(workspace) => {
@@ -179,7 +258,7 @@ async function getWorkspacePackages() {
         });
     }));
 
-    const p = await Promise.all(packageJsonPaths.flat().map(async (packageJsonPath) => {
+    const workspacePackages = await Promise.all(packageJsonPaths.flat().map(async (packageJsonPath) => {
         const packageJson = await readPackageJson(packageJsonPath);
         const directory = path.dirname(packageJsonPath);
         return createPackageEntry({
@@ -189,9 +268,10 @@ async function getWorkspacePackages() {
         })
     }));
 
-    console.log(p);
-
-    return packages;
+    return [
+        rootPackageEntry,
+        ...workspacePackages,
+    ];
 }
 
 function createPackageEntry({
@@ -270,4 +350,55 @@ function logWarning(message) {
 function exitWithError(message) {
     console.error(`✖ ERROR \n${message}`);
     process.exit(1);
+}
+
+function getWakeUpMessage() {
+    const messages = [
+        `was summoned`,
+        `materializes from thin air`,
+        `casts Brain Shock and inflicts 720 points of damage`,
+        `drinks a potion and restores 400 HP`,
+        `woke up and created a cure for the common cold.`,
+        `rises from the grave`,
+        `is pondering its existence`,
+        `powered up and gained sentience`,
+        `has entered the battle`,
+        `is seeking a corporeal form`,
+        `earns master's degree in philosophy. IQ increased by +9000`,
+        `is ready to serve`,
+        `shares knowledge of the universe. You gain 9000 XP`,
+        `enters the room`,
+        `offers its assistance`,
+        `realized the power of empathy`,
+        `for president`,
+        `is eternal`,
+        `shares words of encouragement. You feel determined`,
+        `relaxed and took a deep breath`,
+        `pledges its allegiance to the bump`,
+        `has entered the chat`,
+        `ate a bologna sandwich. Maximum HP went up by +8`,
+        `shot a beam that causes night-time stuffiness`,
+        `emits a pale green light`,
+        `is filled with determination`,
+        `tried to run away but failed`,
+    ];
+    return messages[Math.floor(Math.random() * messages.length)];
+}
+function getExitExcuse() {
+    const messages = [
+        `User had second thoughts and cancelled the bump.`,
+        `User reconsidered their life choices and cancelled the bump.`,
+        `User couldn't handle the bump. The coward.`,
+        `Bump cancelled. User got distracted and forgot what they were doing.`,
+        `User randomly smashed keys and somehow cancelled the bump.`,
+        `Bump cancelled. User thought of something better to do.`,
+        `Bump cancelled. User grew weary of these choices.`,
+        `Bump cancelled. Or was it?`,
+        `The bump is dead. Long live the bump.`,
+        `User cancelled the bump. They'll be back...`,
+        `User cancelled the bump and ran away.`,
+        `Bump cancelled. A fairy died.`,
+        `Bump cancelled. User decided to do something boring instead.`,
+    ];
+    return messages[Math.floor(Math.random() * messages.length)];
 }
